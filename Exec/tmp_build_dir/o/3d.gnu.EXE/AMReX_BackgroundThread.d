d65e0db3b83699c889fa4f2050569bae5ac1db55
prkkumar <prabhatams@gmail.com>
2021-11-05 15:04:33 -0700

    remove temp

--- Exec/tmp_build_dir/o/3d.gnu.EXE/AMReX_BackgroundThread.d
+++
@@ -1,8 +0,0 @@
-tmp_build_dir/o/3d.gnu.EXE/AMReX_BackgroundThread.o: \
- ../../amrex/Src/Base/AMReX_BackgroundThread.cpp \
- ../../amrex/Src/Base/AMReX_BackgroundThread.H \
- tmp_build_dir/s/3d.gnu.EXE/AMReX_Config.H
-
-../../amrex/Src/Base/AMReX_BackgroundThread.H:
-
-tmp_build_dir/s/3d.gnu.EXE/AMReX_Config.H:
--- Exec/tmp_build_dir/o/3d.gnu.EXE/AMReX_Machine.d
+++
@@ -1,5 +0,0 @@
-tmp_build_dir/o/3d.gnu.EXE/AMReX_Machine.o: \
- ../../amrex/Src/Base/AMReX_Machine.cpp \
- tmp_build_dir/s/3d.gnu.EXE/AMReX_Config.H
-
-tmp_build_dir/s/3d.gnu.EXE/AMReX_Config.H:
